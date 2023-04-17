c89631014e5a49720423511f3b25955b68f8f96f
Lena k <ktyecz09@mail.ru>
2023-04-17 08:34:38 +0300

    add initial commit

--- src/test/java/kartatest.java
+++ src/test/java/kartatest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class kartatest {
-}
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class kartatest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(option);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTest() {
+        driver.get("http://localhost:9999/");
+        List<WebElement>inputs=driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("Василий Петров");
+        inputs.get(1).sendKeys("+71111111111");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected ="Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        assertEquals(expected,actual);
+    }
+
+    }
