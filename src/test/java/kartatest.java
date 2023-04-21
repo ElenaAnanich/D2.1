323a0e4d8118a68aa66b5a3c0cdd3dd343129fbc
Lena k <ktyecz09@mail.ru>
2023-04-21 08:52:53 +0300

    add initial commit

--- src/test/java/kartatest.java
+++ src/test/java/kartatest.java
@@ -1,4 +1,5 @@
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +19,7 @@ public class kartatest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -28,6 +29,7 @@ public class kartatest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -38,13 +40,11 @@ public class kartatest {
 
     @Test
     void shouldTest() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id=name]input")).sendKeys("Василий Петров");
-        driver.findElement(By.cssSelector("[data-test-id=phone]input")).sendKeys("+71111111111");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]input")).click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+71111111111");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
-        String expected ="Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-        assertEquals(expected,actual);
+        var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",actualText);
     }
     }
